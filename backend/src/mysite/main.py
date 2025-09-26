1769644b0bdf6f817dc3d5eb8cdc27eedd7284ef
Jie Yang <yangjie8918@gmail.com>
2025-09-26 15:21:11 -0700

    add mongodb and mongo-express

--- backend/src/mysite/main.py
+++ backend/src/mysite/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from uuid import uuid4, UUID
+
+MONGODB_CONNECTION_STRING = os.environ["MONGODB_CONNECTION_STRING"]
+
+client = AsyncIOMotorClient(MONGODB_CONNECTION_STRING, uuidRepresentation="standard")
+db = client.todolist
+todos = db.todos
 
 app = FastAPI()
 
@@ -23,32 +32,26 @@ app.add_middleware(
 )
 
 class TodoItem(BaseModel):
-    id: int
+    # Mongodb uses the id as a primary key
+    id: UUID = Field(default_factory=uuid4, alias="_id")
     content: str
 
 class TodoItemCreate(BaseModel):
     content: str
 
-todos: list[TodoItem] = []
-id_counter = 1
-
 @app.post("/todos", response_model=TodoItem)
 async def create_todo(item: TodoItemCreate):
-    global id_counter
-    new_todo = TodoItem(id=id_counter, content=item.content)
-    todos.append(new_todo)
-    id_counter += 1
+    new_todo = TodoItem(content=item.content)
+    await todos.insert_one(new_todo.model_dump(by_alias=True))
     return new_todo
 
 @app.get("/todos", response_model=list[TodoItem])
 async def read_todos():
-    return todos
+    return await todos.find().to_list(length=None)
 
 @app.delete("/todos/{todo_id}")
-async def delete_todos(todo_id: int):
-    for index, todo in enumerate(todos):
-        if todo.id == todo_id:
-            todos.pop(index)
-            return {"message": "Todo deleted successfully"}
-    raise HTTPException(status_code=404, detail="Todo not found")
-
+async def delete_todos(todo_id: UUID):
+    delete_result = await todos.delete_one({"_id": todo_id})
+    if delete_result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return {"message": "Todo deleted successfully"}
